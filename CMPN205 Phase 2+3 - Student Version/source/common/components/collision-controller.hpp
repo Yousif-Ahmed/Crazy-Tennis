8c8194359c3a61d622a5fb1e2d670e5a5ba89e81
Kariiem <Kariiem@github.com>
2022-06-05 00:18:21 +0200

    ground collision kelaby

--- CMPN205 Phase 2+3 - Student Version/source/common/components/collision-controller.hpp
+++ CMPN205 Phase 2+3 - Student Version/source/common/components/collision-controller.hpp
@@ -1,25 +1,31 @@
 #pragma once
 
-#include "../ecs/component.hpp"
+#include <glm/glm.hpp>
 
-#include <glm/glm.hpp> 
+#include "../ecs/component.hpp"
 
 namespace our {
 
-    // This component denotes that the PlayerControllerSystem will move the owning entity using user inputs.
-    // It will also control the camera field of view accroding to the scrolling of the mouse wheel
-    // This component is added as a slightly complex example for how use the ECS framework to implement logic.
-    // For more information, see "common/systems/player-controller.hpp"
-    // For a more simple example of how to use the ECS framework, see "movement.hpp"
-    class CollisionControllerComponent : public Component {
-    public:
-        glm::vec3 linearVelocity = {3, 3, 3}; // Each frame, the entity should move as follows: position += linearVelocity * deltaTime
-        glm::vec3 direction = {1 ,-1 ,1};  // direction of the collision object 
-        // The ID of this component type is "Free Camera Controller"
-        static std::string getID() { return "Collision Controller"; }
+// This component denotes that the PlayerControllerSystem will move the owning
+// entity using user inputs. It will also control the camera field of view
+// accroding to the scrolling of the mouse wheel This component is added as a
+// slightly complex example for how use the ECS framework to implement logic.
+// For more information, see "common/systems/player-controller.hpp"
+// For a more simple example of how to use the ECS framework, see "movement.hpp"
+class CollisionControllerComponent : public Component {
+ public:
+  glm::vec3 linearVelocity = {
+      -17, -17, -17};  // Each frame, the entity should move as follows: position +=
+                 // linearVelocity * deltaTime
+  glm::vec3 direction = {1, -1, 1};  // direction of the collision object
+  glm::vec3 initialVelocity = {0, 0, 0};
+  int g = 9.8;
 
-        // Reads sensitivities & speedupFactor from the given json object
-        void deserialize(const nlohmann::json& data) override;
-    };
+  ;  // The ID of this component type is "Free Camera Controller"
+  static std::string getID() { return "Collision Controller"; }
 
-}
+  // Reads sensitivities & speedupFactor from the given json object
+  void deserialize(const nlohmann::json& data) override;
+};
+
+}  // namespace our
--- CMPN205 Phase 2+3 - Student Version/source/common/systems/collision-controller.hpp
+++ CMPN205 Phase 2+3 - Student Version/source/common/systems/collision-controller.hpp
@@ -1,83 +1,120 @@
 #pragma once
 
-#include "../ecs/world.hpp"
-#include "../components/collision-controller.hpp"
-
-#include "../application.hpp"
-
 #include <glm/glm.hpp>
 #include <glm/gtc/constants.hpp>
-#include <glm/trigonometric.hpp>
 #include <glm/gtx/fast_trigonometry.hpp>
+#include <glm/trigonometric.hpp>
 
-namespace our
-{
-    // responsible for moving players which have PlayerControllerComponent by the velocity
+#include "../application.hpp"
+#include "../components/collision-controller.hpp"
+#include "../ecs/world.hpp"
 
-    class CollisionControllerSystem
-    {
-        Application *app; // The application in which the state runs
+namespace our {
+// responsible for moving players which have PlayerControllerComponent by the
+// velocity
 
-    public:
-        // When a state enters, it should call this function and give it the pointer to the application
-        void enter(Application *app)
-        {
-            this->app = app;
+class CollisionControllerSystem {
+  Application *app;  // The application in which the state runs
+
+ public:
+  // When a state enters, it should call this function and give it the pointer
+  // to the application
+  void enter(Application *app) { this->app = app; }
+
+  // This should be called every frame to update all entities containing a
+  // FreeCameraControllerComponent
+  void update(World *world, float deltaTime) {
+    CollisionControllerComponent *controller = nullptr;
+
+    Entity *collisionEntity = nullptr;
+    for (auto Currententity : world->getEntities()) {
+      controller = Currententity->getComponent<CollisionControllerComponent>();
+      if (controller) {
+        collisionEntity = Currententity;
+        break;
+      }
+    }
+    // If there is no entity with both a CameraComponent and a
+    // FreeCameraControllerComponent, we can do nothing so we return
+    if (!(collisionEntity && controller)) return;
+
+    // getting the assigned velocity from the controller
+    glm::vec3 &motionVelocity_ref = controller->linearVelocity;
+    auto &gravitationalAcc_ref = controller->g;
+    auto &collisionEntityPosition_ref =
+        collisionEntity->localTransform.position;
+    auto &initialVelocity_ref = controller->initialVelocity;
+    auto direction = controller->direction;
+    auto horizontal_Z = glm::vec3(0, 0, 1);
+    
+    // Change the velocity
+    motionVelocity_ref.y += -gravitationalAcc_ref * deltaTime;
+
+    // r_z = r_z0 + v_z*t ---> delta_r_z = v_z* delta_t
+    collisionEntityPosition_ref.z += motionVelocity_ref.z * deltaTime;
+
+    // r_y = r_y0 + t(v_y0 - 0.5*g*t) ---> delta_r_z = *delta_t
+    collisionEntityPosition_ref.y += initialVelocity_ref.y * deltaTime -
+                                     0.5 * gravitationalAcc_ref * deltaTime;
+
+    // collision detection
+    for (auto Currententity : world->getEntities()) {
+      if (!Currententity->getComponent<CollisionControllerComponent>()) {
+        auto position = Currententity->localTransform.position;
+
+        auto theta = glm::dot(horizontal_Z, controller->direction);
+        bool collisionX =
+            abs(position.x - collisionEntityPosition_ref.x) <= 5.0;
+        bool collisionZ =
+            abs(position.z - collisionEntityPosition_ref.z) <= 5.0;
+        bool collisionY =
+            abs(position.y - collisionEntityPosition_ref.y) <= 1.0;
+
+        bool collisionY_grid =
+            abs(position.y - collisionEntityPosition_ref.y) <= 5.0;
+
+        
+        // collision detection with ground
+        bool groundCollision = collisionY && Currententity->name == "court";
+
+        bool ads_grid_Collision =
+            collisionY_grid && collisionZ &&
+            (Currententity->name == "ads" || Currententity->name == "grid");
+        bool otherEntitiesCollision = collisionX && collisionZ;
+
+        if (ads_grid_Collision) {
+          std::cout << "collision with " << Currententity->name << "\n";
+          gravitationalAcc_ref *= -1;
+          initialVelocity_ref.y = 0.1;
+          initialVelocity_ref.z = 0;
+        }
+        if (groundCollision) {
+          // On Collision the y component of the velocity is reversed
+          gravitationalAcc_ref *= -1;
+          if (motionVelocity_ref.y < 10) {
+            initialVelocity_ref.y = 0;
+            initialVelocity_ref.z = 0;
+          } else {
+            initialVelocity_ref.y = glm::abs(motionVelocity_ref.y) * 0.1;
+            initialVelocity_ref.z = 0.1;
+          }
         }
 
-        // This should be called every frame to update all entities containing a FreeCameraControllerComponent
-        void update(World *world, float deltaTime)
-        {
-            
-            CollisionControllerComponent *controller = nullptr;
-            Entity *collisionEntity = nullptr;
-            for (auto Currententity : world->getEntities())
-            {
-                controller = Currententity->getComponent<CollisionControllerComponent>();
-                if (controller)
-                {
-                    collisionEntity = Currententity;
-                    break;
-                }
-            }
-            // If there is no entity with both a CameraComponent and a FreeCameraControllerComponent, we can do nothing so we return
-            if (!(collisionEntity && controller))
-                return;
+        // else if (otherEntitiesCollision) {  //&& Currententity->name !=
+        //                                        //"grid") {
+        //    //
+        //    std::cout << "collision detected with " << Currententity->name
+        //              << std::endl;
+        //    g = -g;
+        //    controller->initialVelocity.y = -controller->linearVelocity.y;
+        //    controller->initialVelocity.z = -controller->linearVelocity.z;
+        //  }
+        //  if (collisionPosition.y > 20) {
+        //    velocity.y = glm::abs(velocity.y);
+        //  }
+      }
+    }
+  }
+};
 
-            // getting the assigned velocity from the controller
-            glm::vec3 velocity = controller->linearVelocity;
-            auto collisionPostion = collisionEntity->localTransform.position;
-            auto direction = controller->direction;
-            collisionEntity->localTransform.position.z -=direction.z* velocity.z *deltaTime ;
-            collisionEntity->localTransform.position.y += direction.y *deltaTime ;
-            // collision detection
-            for (auto Currententity : world->getEntities())
-            {
-                if (!Currententity->getComponent<CollisionControllerComponent>()&&!Currententity->getComponent<CameraComponent>())
-                {
-                    auto position = Currententity->localTransform.position;
-                    bool collisionX = abs(position.x  - collisionPostion.x) <= 1.0 ;
-                    bool collisionZ = abs(position.z  - collisionPostion.z) <= 1.0;
-                    bool collisiony = abs(position.y  - collisionPostion.y) <= 1.0;
-
-                    // std::cout <<(abs(position.y  - collisionPostion.y))<< "  y  "<<std::endl;
-                    // std::cout <<(abs(position.z  - collisionPostion.z))<< " z "<<std::endl;
-                    std::cout <<(abs(position.y  - collisionPostion.y) )<<std::endl ;
-                    // collision detection with ground
-                    if (collisiony &&  Currententity->name == "court"  )
-                    {
-                        std::cout << "collision detected with " <<Currententity->name<<std::endl;
-                        // if ( collisionPostion.y <= 10)
-                        // {
-                            controller->direction.y = -1 *controller->direction.y;
-
-                        // }
-
-                        //controller->direction = - direction;
-                    }
-                }
-            }
-        }
-    };
-
-}
+}  // namespace our
