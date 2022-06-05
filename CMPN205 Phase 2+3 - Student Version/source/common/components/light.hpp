75c45c300c81a562049af71cbe2ec08525c3033f
Yahia Zakaria <yahiazakaria3200@gmail.com>
2022-06-05 15:46:02 +0200

    sperate sky light parameters

--- CMPN205 Phase 2+3 - Student Version/source/common/components/light.hpp
+++ CMPN205 Phase 2+3 - Student Version/source/common/components/light.hpp
@@ -18,6 +18,10 @@ namespace our{
         glm::vec3 position = glm::vec3(1.0f);
         glm::vec3 direction = glm::vec3(1.0f);
 
+        glm::vec3 top = glm::vec3(1.0f);
+        glm::vec3 middle = glm::vec3(0.6f);
+        glm::vec3 bottom = glm::vec3(0.3f);
+
         static std::string getID(){return "Light";}
 
         void deserialize(const nlohmann::json& data) override;
