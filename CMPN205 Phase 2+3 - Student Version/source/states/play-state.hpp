2fc39a751309524ecf89d61a7f37a2c3a1b57395
Yousif Ahmed <56611494+Yousif-Ahmed@users.noreply.github.com>
2022-06-05 22:18:47 +0200

    editing main menu

--- CMPN205 Phase 2+3 - Student Version/source/states/play-state.hpp
+++ CMPN205 Phase 2+3 - Student Version/source/states/play-state.hpp
@@ -61,24 +61,24 @@ class Playstate: public our::State {
     }
     void onImmediateGui() override
     {
-        ImGui::Begin("score" , 0 , ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove);
+        ImGui::Begin("score" , 0 ,  ImGuiWindowFlags_NoMove);
         // setting window position 
         ImGui::SetWindowPos(ImVec2(950,50));
         // setting window size
-        ImGui::SetWindowSize(ImVec2(150,50));
+        ImGui::SetWindowSize(ImVec2(150,80));
 
         // writing text to window 
-        ImGui::TextColored(ImVec4(1.0f,1.0f,0.0f,1.0f) , "Player 1 | Score : %d" , getApp()->player1_score);
+        ImGui::TextColored(ImVec4(1.0f,1.0f,0.0f,1.0f) , "Player 1   : %d" , getApp()->player1_score);
         ImGui::End();
 
-        ImGui::Begin("score" , 0 , ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove);
+        ImGui::Begin("score" , 0 ,   ImGuiWindowFlags_NoMove);
         // setting window position 
         ImGui::SetWindowPos(ImVec2(950,50));
         // setting window size
-        ImGui::SetWindowSize(ImVec2(175,50));
+        ImGui::SetWindowSize(ImVec2(175,80));
 
         // writing text to window 
-        ImGui::TextColored(ImVec4(1.0f,1.0f,0.0f,1.0f) , "Player 2 | Score : %d" , getApp()->player2_score);
+        ImGui::TextColored(ImVec4(1.0f,1.0f,0.0f,1.0f) , "Player 2   : %d" , getApp()->player2_score);
         
         ImGui::End();
     }
